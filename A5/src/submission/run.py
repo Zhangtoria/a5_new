dca816723ebb7028dbfd30f93417d58a3008b7b4
Zhangtoria <47379778+Zhangtoria@users.noreply.github.com>
2021-09-03 21:18:29 +1000

    Update run.py

--- A5/src/submission/run.py
+++ A5/src/submission/run.py
@@ -165,9 +165,12 @@ elif args.function == 'finetune':
     text = dataset.NameDataset(open(args.finetune_corpus_path).read(),pretrain_dataset)
     
     if args.reading_params_path is None:
+        model = model.GPT(mconf)
+        model.to(device)
         my_trainer = trainer.Trainer(model,text,None,tconf)
     else: 
         model = model.load_state_dict(torch.load(args.reading_params_path))
+        model.to(device)
         my_trainer = trainer.Trainer(model,text,None,tconf)
     
     my_trainer.train()
